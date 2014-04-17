56d5108093796d04e232597c08806176e45fa33b
martin <martin@zippyrate.com>
2014-04-16 17:51:45 -0700

    Added more fields to series DTO

--- src/main/java/edu/stanford/epad/dtos/EPADSeries.java
+++ src/main/java/edu/stanford/epad/dtos/EPADSeries.java
@@ -14,12 +14,16 @@ public class EPADSeries implements Serializable
 	private static final long serialVersionUID = 1L;
 
 	public final String studyUID, seriesUID, patientID, patientName;
-	public final String seriesDate, seriesDescription, examType, bodyPart;
-	public final int numberOfImages;
+	public final String seriesDate, seriesDescription, examType, bodyPart, accessionNumber;
+	public final int numberOfImages, numberOfSeriesRelatedInstances;
 	public final int numberOfAnnotations;
+	public final String institution, stationName, department;
+	public final int pngProcessingStatus;
 
 	public EPADSeries(String studyUID, String seriesUID, String patientID, String patientName, String seriesDate,
-			String seriesDescription, String examType, String bodyPart, int numberOfImages, int numberOfAnnotations)
+			String seriesDescription, String examType, String bodyPart, String accessionNumber, int numberOfImages,
+			int numberOfSeriesRelatedInstances, int numberOfAnnotations, String institution, String stationName,
+			String department, int pngProcessingStatus)
 	{
 		this.studyUID = studyUID;
 		this.seriesUID = seriesUID;
@@ -29,8 +33,14 @@ public class EPADSeries implements Serializable
 		this.seriesDescription = seriesDescription;
 		this.examType = examType;
 		this.bodyPart = bodyPart;
+		this.accessionNumber = accessionNumber;
 		this.numberOfImages = numberOfImages;
+		this.numberOfSeriesRelatedInstances = numberOfSeriesRelatedInstances;
 		this.numberOfAnnotations = numberOfAnnotations;
+		this.institution = institution;
+		this.stationName = stationName;
+		this.department = department;
+		this.pngProcessingStatus = pngProcessingStatus;
 	}
 
 	public String toJSON()
