a0866b573c6f7599fa476cb9dcfa84666dc1149b
dgude <dev.gude@gmail.com>
2014-08-25 11:25:27 -0700

    Added username

--- src/main/java/edu/stanford/epad/dtos/EPADAIM.java
+++ src/main/java/edu/stanford/epad/dtos/EPADAIM.java
@@ -9,13 +9,15 @@ public class EPADAIM implements Serializable
 	private static final long serialVersionUID = 1L;
 
 	public final String aimID;
+	public final String userName;
 	public final String projectID, subjectID, studyUID, seriesUID, imageUID;
 	public final int instanceOrFrameNumber;
 
-	public EPADAIM(String aimID, String projectID, String subjectID, String studyUID, String seriesUID, String imageUID,
+	public EPADAIM(String aimID, String userName, String projectID, String subjectID, String studyUID, String seriesUID, String imageUID,
 			int instanceOrFrameNumber)
 	{
 		this.aimID = aimID;
+		this.userName = userName;
 		this.projectID = projectID;
 		this.subjectID = subjectID;
 		this.studyUID = studyUID;
