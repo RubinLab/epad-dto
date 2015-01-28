1e70287c7a3fd16e866f636afd74361b65a7f7ca
dgude <dev.gude@gmail.com>
2015-01-28 13:45:40 -0800

    Added password expired flag

--- src/main/java/edu/stanford/epad/dtos/EPADUser.java
+++ src/main/java/edu/stanford/epad/dtos/EPADUser.java
@@ -17,13 +17,13 @@ public class EPADUser implements Serializable
 
 	public final String displayname, username, firstname, lastname, email;
 	public final String role; // Project specific role only
-	public final boolean enabled, admin;
+	public final boolean enabled, admin, passwordExpired;
 	public final Set<String> permissions;
 	public final EPADMessageList messages;
 	public final Set<String> projects;
 	public final Map<String,String> projectToRole;
 
-	public EPADUser(String displayname, String username, String firstname, String lastname, String email, boolean enabled, boolean admin, String role, Set<String> permissions)
+	public EPADUser(String displayname, String username, String firstname, String lastname, String email, boolean enabled, boolean admin, boolean passwordExpired, String role, Set<String> permissions)
 	{
 		this.displayname = displayname;
 		this.username = username;
@@ -32,6 +32,7 @@ public class EPADUser implements Serializable
 		this.email = email;
 		this.enabled = enabled;
 		this.admin = admin;
+		this.passwordExpired = passwordExpired;
 		this.role = role;
 		this.permissions = permissions;
 		this.messages = null;
@@ -39,7 +40,7 @@ public class EPADUser implements Serializable
 		this.projectToRole = null;
 	}
 
-	public EPADUser(String displayname, String username, String firstname, String lastname, String email, boolean enabled, boolean admin, String role, Set<String> permissions, Set<String> projects, Map<String, String> projectToRole, EPADMessageList messages)
+	public EPADUser(String displayname, String username, String firstname, String lastname, String email, boolean enabled, boolean admin, boolean passwordExpired, String role, Set<String> permissions, Set<String> projects, Map<String, String> projectToRole, EPADMessageList messages)
 	{
 		this.displayname = displayname;
 		this.username = username;
@@ -48,6 +49,7 @@ public class EPADUser implements Serializable
 		this.email = email;
 		this.enabled = enabled;
 		this.admin = admin;
+		this.passwordExpired = passwordExpired;
 		this.role = role;
 		this.permissions = permissions;
 		this.projects = projects;
