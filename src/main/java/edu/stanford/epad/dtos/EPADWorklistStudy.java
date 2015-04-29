cc93601ddedcef683ec25362ba90d2d23a98a0d7
dgude <dev.gude@gmail.com>
2015-04-29 16:18:22 -0700

    Added studyUID

--- src/main/java/edu/stanford/epad/dtos/EPADWorklistStudy.java
+++ src/main/java/edu/stanford/epad/dtos/EPADWorklistStudy.java
@@ -16,17 +16,18 @@ public class EPADWorklistStudy implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 
-	public final String workListID, username, projectID, subjectID, status;
+	public final String workListID, username, projectID, studyUID, subjectID, status;
 	
 	public final String startDate, completionDate;
 
 	public EPADWorklistStudy(String workListID, String username, String projectID, String subjectID,
-			String status, String startDate,
+			String studyUID, String status, String startDate,
 			String completionDate) {
 		super();
 		this.workListID = workListID;
 		this.username = username;
 		this.projectID = projectID;
+		this.studyUID = studyUID;
 		this.subjectID = subjectID;
 		this.status = status;
 		this.startDate = startDate;
