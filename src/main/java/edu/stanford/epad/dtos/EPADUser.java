4484dbad834e6fe31b0b7e877eca19df63b8ecf9
dgude <dev.gude@gmail.com>
2015-10-19 15:05:33 -0700

    Remove obsolete logs

--- src/main/java/edu/stanford/epad/dtos/EPADUser.java
+++ src/main/java/edu/stanford/epad/dtos/EPADUser.java
@@ -19,7 +19,6 @@ public class EPADUser implements Serializable
 	public final String role; // Project specific role only
 	public final boolean enabled, admin, passwordExpired;
 	public final Set<String> permissions;
-	public final EPADMessageList messages;
 	public final Set<String> projects;
 	public final List<String> projectToRole;
 	public String colorpreference;
@@ -36,12 +35,11 @@ public class EPADUser implements Serializable
 		this.passwordExpired = passwordExpired;
 		this.role = role;
 		this.permissions = permissions;
-		this.messages = null;
 		this.projects = null;
 		this.projectToRole = null;
 	}
 
-	public EPADUser(String displayname, String username, String firstname, String lastname, String email, boolean enabled, boolean admin, boolean passwordExpired, String role, Set<String> permissions, Set<String> projects, List<String> projectToRole, EPADMessageList messages)
+	public EPADUser(String displayname, String username, String firstname, String lastname, String email, boolean enabled, boolean admin, boolean passwordExpired, String role, Set<String> permissions, Set<String> projects, List<String> projectToRole)
 	{
 		this.displayname = displayname;
 		this.username = username;
@@ -55,7 +53,6 @@ public class EPADUser implements Serializable
 		this.permissions = permissions;
 		this.projects = projects;
 		this.projectToRole = projectToRole;
-		this.messages = messages;
 	}
 
 	public String toJSON()
